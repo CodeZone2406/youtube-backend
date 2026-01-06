a0c6b3018bb2d9ba8d13061e1fe3de54b6bbd925
Arbaaz Ansari <ansarbaaz@hotmail.com>
2026-01-06 20:12:34 +0300

    Add subscription model with schema for subscriber and channel

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -211,7 +211,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
             secure: true
         }
     
-        const { accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+        const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
     
         return res
         .status(200)
